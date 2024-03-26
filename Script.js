3ecd852b874a18c2a6196f43186cad0fcc1b523c
Eduardo Vargas C <159199172+EduardoVargas1225@users.noreply.github.com>
2024-03-26 11:46:34 -0500

    Main
    Se le dio formato a los boxes del main, se le agrego color y disieño final a la pagina principal

---
+++ Script.js
@@ -0,0 +1,13 @@
+const boxes = document.querySelectorAll('.box');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    }
+  });
+});
+
+boxes.forEach(box => {
+  observer.observe(box);
+});
